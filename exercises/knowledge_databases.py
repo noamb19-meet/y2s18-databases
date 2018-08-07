02a4a591519c2c3000f8b2a0e4051e701aa90209
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 12:13:53 +0300

    hi

--- exercises/knowledge_databases.py
+++ exercises/knowledge_databases.py
@@ -36,13 +36,16 @@ def delete_all_articles():
 	
 	session.commit()
 
-def edit_article_rating():
-
-	pass
-
+def edit_article_rating(updated_rating,article_title):
+	article_object=session.query(Knowledge).filter_by(
+		title=article_title).first()
+	article_object.rating=updated_rating
+	session.commit()
 #add_article("dogs","all about dogs",8)
 #print(query_all_articles())
 
 #print(query_article_by_topic("cats"))
 #delete_article_by_topic("dogs")
-delete_all_articles()
+#delete_all_articles()
+edit_article_rating(10,"all about dogs")
+print(query_all_articles())
